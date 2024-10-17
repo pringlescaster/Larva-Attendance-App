14c3778d88336a3ff03849a452eef4c825d93551
DAVID <pringlescaster@gmail.com>
2024-10-17 18:28:20 +0100

    updated the register student to add image

--- src/app/dashboard/registerstudent/page.js
+++ src/app/dashboard/registerstudent/page.js
@@ -16,7 +16,7 @@ function Page() {
   const [showSuccessModal, setShowSuccessModal] = useState(false);
   const [image, setImage] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
-
+  
   const [studentData, setStudentData] = useState({
     name: "",
     studentnumber: "",
@@ -38,7 +38,7 @@ function Page() {
   const handleImageChange = (e) => {
     const selectedImage = e.target.files[0];
     setImage(selectedImage);
-    if (selectedImage){
+    if (selectedImage) {
       const imageUrl = URL.createObjectURL(selectedImage);
       setImagePreview(imageUrl);
     }
@@ -66,33 +66,51 @@ function Page() {
         formData.append("image", image);
       }
 
-      const response = await axios.post(
-        "https://larva-attendance-app-server.vercel.app/api/v1/student/register",
+      const imageUploadResponse = await axios.post(
+        "http://localhost:2000/api/v1/upload", 
         formData,
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            'Content-Type': 'multipart/form-data',
+            "Content-Type": "multipart/form-data",
           },
         }
       );
 
-      if (response.status === 201) {
-        setMessage("Student registered successfully");
-        setStudentData({
-          name: "",
-          studentnumber: "",
-          course: "",
-          cohort: "",
-        });
-        setImage(null);
-        setImagePreview(null);
-        setShowSuccessModal(true);
-        setTimeout(() => {
-          setShowSuccessModal(false);
-        }, 3000);
-      } else {
-        setMessage(response.data.message || "Error registering student");
+      if (imageUploadResponse.status === 200) {
+        const response = await axios.post(
+          "https://larva-attendance-app-server.vercel.app/api/v1/student/register",
+          {
+            name: studentData.name,
+            studentnumber: studentData.studentnumber,
+            course: studentData.course,
+            cohort: studentData.cohort,
+            image: imageUploadResponse.data.imageUrl // Assuming the uploaded image URL is returned
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            },
+          }
+        );
+
+        if (response.status === 201) {
+          setMessage("Student registered successfully");
+          setStudentData({
+            name: "",
+            studentnumber: "",
+            course: "",
+            cohort: "",
+          });
+          setImage(null);
+          setImagePreview(null);
+          setShowSuccessModal(true);
+          setTimeout(() => {
+            setShowSuccessModal(false);
+          }, 3000);
+        } else {
+          setMessage(response.data.message || "Error registering student");
+        }
       }
     } catch (error) {
       console.error("Error registering student:", error);
@@ -140,17 +158,22 @@ function Page() {
                   className="w-full space-y-4 text-sm"
                 >
                   <div className="flex justify-center items-center flex-col gap-y-2">
-                    { imagePreview ? (
+                    {imagePreview ? (
                       <Image
-                      className="w-[90px]"
-                      src={imagePreview}
-                      alt="Uploaded Profile"
-                    /> ) : (
+                        className="w-[90px]"
+                        src={imagePreview}
+                        alt="Uploaded Profile"
+                        height={90}
+                        width={90}
+                      />
+                    ) : (
                       <Image
-                      className="w-[90px]"
-                      src={registerProfile}
-                      alt="Register Profile"
-                    />
+                        className="w-[90px]"
+                        src={registerProfile}
+                        alt="Register Profile"
+                        width={90}
+                        height={90}
+                      />
                     )}
 
                     {/* Label for file input */}
@@ -165,6 +188,7 @@ function Page() {
                       id="fileInput"
                       className="hidden" // Hide the actual input
                       type="file"
+                      name="image"
                       accept="image/*"
                       onChange={handleImageChange}
                     />
@@ -246,57 +270,54 @@ function Page() {
           />
         ) : (
           <div className="py-8 gap-y-6 px-8 flex flex-col items-center">
-             <form
+            <form
               onSubmit={handleSubmit}
               className="px-4 text-sm w-screen grid gap-y-4"
             >
-            <div className="flex flex-col gap-y-2">
-              { imagePreview ? (
-              <Image
-                className="w-[90px]"
-                src={imagePreview}
-                alt="Uploaded Profile"
-              /> ) : ( <Image
-                className="w-[90px]"
-                src={registerProfile}
-                alt="Register Profile"
-              />
-              )}
-              
-               {/* Label for file input */}
-               <label
-                      className="text-[#f39b3b] font-semibold cursor-pointer"
-                      htmlFor="fileInput"
-                    >
-                      Add Photo
-                    </label>
+              <div className="flex justify-center items-center flex-col gap-y-2">
+                {imagePreview ? (
+                  <Image
+                    className="w-[90px]"
+                    src={imagePreview}
+                    alt="Uploaded Profile"
+                    height={90}
+                    width={90}
+                  />
+                ) : (
+                  <Image
+                    className="w-[90px]"
+                    src={registerProfile}
+                    alt="Register Profile"
+                    width={90}
+                    height={90}
+                  />
+                )}
 
-                    <input
-                      id="fileInput"
-                      className="hidden" // Hide the actual input
-                      type="file"
-                      accept="image/*"
-                      onChange={handleImageChange}
-                    />
-            </div>
-           
+                <label
+                  className="text-[#f39b3b] font-semibold cursor-pointer"
+                  htmlFor="fileInput"
+                >
+                  Add Photo
+                </label>
+
+                <input
+                  id="fileInput"
+                  className="hidden"
+                  type="file"
+                  name="image"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                />
+              </div>
+
               <input
                 className="w-full rounded-lg outline-[#F39B3B] text-[#222222] hover:border-[#F39B3B] bg-[#F9F9F9] px-4 py-3 border border-[#D3D3D3]"
                 type="text"
                 name="name"
                 value={studentData.name}
                 onChange={handleChange}
+                required
                 placeholder="Name"
-                required
-              />
-              <input
-                className="w-full rounded-lg outline-[#F39B3B] text-[#222222] hover:border-[#F39B3B] bg-[#F9F9F9] px-4 py-3 border border-[#D3D3D3]"
-                type="number"
-                name="studentnumber"
-                value={studentData.studentnumber}
-                onChange={handleChange}
-                placeholder="Student Number"
-                required
               />
               <input
                 className="w-full rounded-lg outline-[#F39B3B] text-[#222222] hover:border-[#F39B3B] bg-[#F9F9F9] px-4 py-3 border border-[#D3D3D3]"
@@ -307,15 +328,28 @@ function Page() {
                 placeholder="Course"
                 required
               />
-              <input
-                className="w-full rounded-lg outline-[#F39B3B] text-[#222222] hover:border-[#F39B3B] bg-[#F9F9F9] px-4 py-3 border border-[#D3D3D3]"
-                type="text"
-                name="cohort"
-                value={studentData.cohort}
-                onChange={handleChange}
-                placeholder="Cohort"
-                required
-              />
+
+              <div className="flex gap-x-[16px]">
+                <input
+                  className="w-full rounded-lg outline-[#F39B3B] text-[#222222] hover:border-[#F39B3B] bg-[#F9F9F9] px-4 py-3 border border-[#D3D3D3]"
+                  type="number"
+                  name="studentnumber"
+                  value={studentData.studentnumber}
+                  onChange={handleChange}
+                  placeholder="Student Number"
+                  required
+                />
+                <input
+                  className="w-full rounded-lg outline-[#F39B3B] text-[#222222] hover:border-[#F39B3B] bg-[#F9F9F9] px-4 py-3 border border-[#D3D3D3]"
+                  type="text"
+                  name="cohort"
+                  value={studentData.cohort}
+                  onChange={handleChange}
+                  placeholder="Cohort"
+                  required
+                />
+              </div>
+
               <button
                 type="submit"
                 className="bg-[#f39b3b] mt-6 w-full py-3 px-4 rounded-md text-white text-md font-base"
@@ -323,6 +357,8 @@ function Page() {
                 Register Student
               </button>
             </form>
+
+            {message && <p className="text-center mt-4">{message}</p>}
           </div>
         )}
       </div>
